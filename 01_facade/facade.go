e626838c932c7967e3281d620a7f9401e43621da
cuijxin <cuijxin@126.com>
2020-04-26 02:33:12 +0000

    facade update

--- 01_facade/facade.go
+++ 01_facade/facade.go
@@ -2,14 +2,29 @@ package facade
 
 import "fmt"
 
+func NewAPI() API {
+	return &apiImpl{
+		a: NewAModuleAPI(),
+		b: NewBModuleAPI(),
+	}
+}
+
 // API is facade interface of facade package
 type API interface {
 	Test() string
 }
 
 // facade implement
-type apiTmpl struct {
-	
+type apiImpl struct {
+	a AModuleAPI
+	b BModuleAPI
+}
+
+func (a *apiImpl) Test() string {
+	aRet := a.a.TestA()
+	bRet := a.b.TestB()
+
+	return fmt.Sprintf("%s\n%s", aRet, bRet)
 }
 
 // NewAModuleAPI return new AModuleAPI
@@ -22,10 +37,24 @@ type AModuleAPI interface {
 	TestA() string
 }
 
-type aModuleImpl struct {
-
-}
+type aModuleImpl struct {}
 
 func (*aModuleImpl) TestA() string {
 	return "A module running"
+}
+
+// NewBModuleAPI return new BModuleAPI
+func NewBModuleAPI() BModuleAPI {
+	return &bModuleImpl{}
+}
+
+// BModuleAPI ...
+type BModuleAPI interface {
+	TestB() string
+}
+
+type bModuleImpl struct {}
+
+func (*bModuleImpl) TestB() string {
+	return "B module running"
 }
